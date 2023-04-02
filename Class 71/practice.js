7dc94dee13821db734aaa2854012d97030a9550d
RHS-Rabbi <rabbihossain187@gmail.com>
2023-04-02 14:55:20 +0600

    Class 71

---
+++ Class 71/practice.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+    // $('[data-toggle="popover"]').popover()
+  })
+  });
+  $(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+});
