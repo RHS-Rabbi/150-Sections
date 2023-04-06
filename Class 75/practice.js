3f407620d1b55c4117f2b0a4ebd88217c1d5ea8f
Mohammad Rabbi Hossain <rabbihossain187@gmail.com>
2023-04-06 16:09:20 +0600

    Class 75

---
+++ Class 75/practice.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#bar1').barfiller({ barColor:'#fff', duration:'4000'});
+    $('#bar2').barfiller({ barColor:'#fff', duration:'4000'});
+    $('#bar3').barfiller({ barColor:'#fff', duration:'4000'});
+    $('#bar4').barfiller({ barColor:'#fff', duration:'4000'});
+
+});
