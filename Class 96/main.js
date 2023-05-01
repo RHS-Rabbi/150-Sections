7c78da5f05c87e9802e3d97636b8186a089d4dee
Mohammad Rabbi Hossain <rabbihossain187@gmail.com>
2023-05-01 17:21:23 +0600

    Class 96

---
+++ Class 96/main.js
@@ -0,0 +1,23 @@
+$('#main-slider  .owl-carousel').owlCarousel(
+  {
+    items: 1,
+    margin: 0,
+    loop: true,
+    nav: false,
+    navText: ['Back','Next'],
+    dots: true,
+    autoplay: true,
+  }
+);
+
+
+$('#logo-area  .owl-carousel').owlCarousel(
+  {
+    items: 5,
+    margin: 0,
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+  }
+);
