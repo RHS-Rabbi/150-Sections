0d289ce6798fe07d483fb1f49285468d5ec4b224
Mohammad Rabbi Hossain <rabbihossain187@gmail.com>
2023-05-02 21:13:56 +0600

    Class 100

---
+++ Class 100/main.js
@@ -0,0 +1,8 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav: true,
+    loop: true,
+    items:3,
+    navText: ['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+})
