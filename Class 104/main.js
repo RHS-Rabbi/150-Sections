9975484249ba759c6497697a622f231d49e3c230
Mohammad Rabbi Hossain <rabbihossain187@gmail.com>
2023-05-03 20:01:05 +0600

    Class 104

---
+++ Class 104/main.js
@@ -0,0 +1,8 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    items:4,
+    navText: ['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+  })
