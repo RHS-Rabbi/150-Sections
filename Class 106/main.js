8a88ce67376ab44df3ce73ba53b13e807a4a8308
Mohammad Rabbi Hossain <rabbihossain187@gmail.com>
2023-05-04 16:00:05 +0600

    Class 106

---
+++ Class 106/main.js
@@ -0,0 +1,8 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    items: 1,
+    navText: ['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+    autoplay: true,
+})
