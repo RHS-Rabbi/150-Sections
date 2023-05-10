901c71f18c6971fcd746cbe8fe3051e51d6618b4
Mohammad Rabbi Hossain <rabbihossain187@gmail.com>
2023-05-10 17:50:11 +0600

    Class 118

---
+++ Class 118/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    items:5,
+    autoPlay: true,
+    dots: true,
+    nav:false,
+    margin:0,
+})
+  
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  
