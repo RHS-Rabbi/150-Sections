4397ff78eb170fd391f0e429b33cdf071ecbecb4
Mohammad Rabbi Hossain <rabbihossain187@gmail.com>
2023-05-16 18:32:56 +0600

    Class 128

---
+++ Class 128/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    // $(".video-play-btn").modalVideo();
+ 
+});
+
+$(document).ready(function() {
+    $('.video-play-btn, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+});
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
